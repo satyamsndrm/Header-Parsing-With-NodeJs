3e2fec2b4f800a674c2d710d9c0e833f0e665865
Glitch (thirsty-mailman) <none>
2018-06-25 05:18:26 +0000

    🎷🎭 Checkpoint ./server.js:1276971/237

--- server.js
+++ server.js
@@ -24,6 +24,11 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+app.get('/api/whoami/' , (req , res) => {
+  console.log(req);
+  res.json({ipaddress:req.ip , language: req.get('Accept-Language') , software:req.get('User-Agent')});
+});
+
 
 
 // listen for requests :)

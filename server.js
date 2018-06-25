29d46b333cafd2698c804df3bf7b11b5bd19ea26
Glitch (thirsty-mailman) <none>
2018-06-25 05:22:42 +0000

    Initial Commit

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ app.get("/api/hello", function (req, res) {
 });
 
 app.get('/api/whoami/' , (req , res) => {
-  console.log(req);
+  //console.log(req);
   res.json({ipaddress:req.ip , language: req.get('Accept-Language') , software:req.get('User-Agent')});
 });
 
